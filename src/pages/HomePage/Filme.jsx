10b644e479e7c48e79e28b92cd2639e54dea5024
Luiz Fernando <113379005+luizfernando2644@users.noreply.github.com>
2024-09-19 14:46:16 -0300

    melhora componente filme

--- src/pages/HomePage/Filme.jsx
+++ src/pages/HomePage/Filme.jsx
@@ -1,7 +1,15 @@
+import styled from "styled-components"
+
 export default function Filme({ filme }) {
     return (
         <div>
             <img src={filme.posterURL} alt={filme.title} />
+            <TituloFilme>{filme.title}</TituloFilme>
         </div>
     )
 }
+
+const TituloFilme = styled.div`
+    font-size: 15px;
+    font-weight: bold;
+`
